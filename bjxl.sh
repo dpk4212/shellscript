5e490e31b1a54ffa48c2cfc4d247b63614ba5071
dpk4212 <dpk.forum@gmail.com>
2023-10-01 02:22:52 +0700

    add list supported image format

--- bjxl.sh
+++ bjxl.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# just a script to convert all image to jxl
+# require imagemagick, darktable, exiftool
+# available parameter
+# -r recursive , include all sub directories
+# -del delete source file 
+# -q=xx image quality
 
 # Function to parse command line arguments and extract attributes and their values
 quality=0
@@ -63,59 +69,71 @@ fileConvert()
     fext="${i##*.}"
 
     case "$fext" in
+      #this ext list base on image supported by cjxl
       PNG|APNG|GIF|JPEG|JPG|EXR|PPM|PFM|PGX|png|apng|gif|jpeg|jpg|exr|ppm|pfm|pgx)
         extconvert=0
         ;;
-        3FR|ARI|ARW|BAY|BMQ|CAP|CINE|CR2|CR3|CRW|CS1|DC2|DCR|DNG|GPR|ERF|FFF|EXR|IA|IIQ|K25|KC2|KDC|MDC|MEF|MOS|MRW|NEF|NRW|ORF|PEF|PFM|PXN|QTK|RAF|RAW|RDC|RW1|RW2|SR2|SRF|SRW|STI|X3F|3fr|ari|arw|bay|bmq|cap|cine|cr2|cr3|crw|cs1|dc2|dcr|dng|gpr|erf|fff|exr|ia|iiq|k25|kc2|kdc|mdc|mef|mos|mrw|nef|nrw|orf|pef|pfm|pxn|qtk|raf|raw|rdc|rw1|rw2|sr2|srf|srw|sti|x3f)
-          
-          #extract jpeg preview if available
-          echo exiftool -b -JpgFromRaw "$i" 
-          exiftool -b -JpgFromRaw "$i" > "#_tmps_${fname}.jpg"
 
-          #if file size 0 raw don't have preview
-          if [ ! -s "#_tmps_${fname}.jpg" ]; then 
+      #this ext list base on image supported by darktable
+      3FR|ARI|ARW|BAY|BMQ|CAP|CINE|CR2|CR3|CRW|CS1|DC2|DCR|DNG|GPR|ERF|FFF|EXR|IA|IIQ|K25|KC2|KDC|MDC|MEF|MOS|MRW|NEF|NRW|ORF|PEF|PFM|PXN|QTK|RAF|RAW|RDC|RW1|RW2|SR2|SRF|SRW|STI|X3F|3fr|ari|arw|bay|bmq|cap|cine|cr2|cr3|crw|cs1|dc2|dcr|dng|gpr|erf|fff|exr|ia|iiq|k25|kc2|kdc|mdc|mef|mos|mrw|nef|nrw|orf|pef|pfm|pxn|qtk|raf|raw|rdc|rw1|rw2|sr2|srf|srw|sti|x3f)    
+
+        #extract jpeg preview if available
+        echo exiftool -b -JpgFromRaw "$i" 
+        exiftool -b -JpgFromRaw "$i" > "#_tmps_${fname}.jpg"
+
+        #if file size 0 raw don't have preview
+        if [ ! -s "#_tmps_${fname}.jpg" ]; then 
+           rm "#_tmps_${fname}.jpg"
+        else
+          #check preview size, sometime it only a thumbnail
+           ori_width=$(identify -format "%w" "$i")
+           ori_width=$((ori_width - 32))
+           preview_width=$(identify -format "%w" "#_tmps_${fname}.jpg")
+           
+           if [ $ori_width -gt $preview_width  ]; then
              rm "#_tmps_${fname}.jpg"
-          else
-            #check preview size, sometime it only a thumbnail
-             ori_width=$(identify -format "%w" "$i")
-             ori_width=$((ori_width - 32))
-             preview_width=$(identify -format "%w" "#_tmps_${fname}.jpg")
-             
-             if [ $ori_width -gt $preview_width  ]; then
-               rm "#_tmps_${fname}.jpg"
-             fi
+           fi
+        fi
+
+        if [ -f "#_tmps_${fname}.jpg" ]; then
+          #most of preview do not include exif, copy exif from raw
+          exiftool -tagsfromfile "$i" -all:all "#_tmps_${fname}.jpg" >/dev/null 2>/dev/null
+        else 
+          #convert to jpg using darktable 
+          echo $darktable "$i" "#_tmps_${fname}.jpg" --icc-type SRGB
+          $darktable "$i" "#_tmps_${fname}.jpg" --icc-type SRGB 2>/dev/null
+
+          #if conversion fail remove temp file
+          if [ $? -ne 0 ]; then rm "#_tmps_${fname}.jpg" 2>/dev/null ;fi
+        fi
+
+        if [ -f "#_tmps_${fname}.jpg" ]; then
+          if [ $deletefile -eq 1 ]; then
+            rm "$i"
           fi
-
-          if [ -f "#_tmps_${fname}.jpg" ]; then
-            #most of preview do not include exif, copy exif from raw
-            exiftool -tagsfromfile "$i" -all:all "#_tmps_${fname}.jpg" >/dev/null 2>/dev/null
-          else 
-            #convert to jpg using darktable 
-            echo $darktable "$i" "#_tmps_${fname}.jpg" --icc-type SRGB
-            $darktable "$i" "#_tmps_${fname}.jpg" --icc-type SRGB 2>/dev/null
-
-            #if conversion fail remove temp file
-            if [ $? -ne 0 ]; then rm "#_tmps_${fname}.jpg" 2>/dev/null ;fi
-          fi
-
-          if [ -f "#_tmps_${fname}.jpg" ]; then
-            if [ $deletefile -eq 1 ]; then
-              rm "$i"
-            fi
-            i="#_tmps_${fname}.jpg"
-          else 
-            #conversion fail skip the job
-            continue
-          fi
-
-          extconvert=0
-        ;;
-        JXL|jxl)
+          i="#_tmps_${fname}.jpg"
+        else 
+          #conversion fail skip the job
           continue
-        ;;
-        *)
+        fi
+
+        extconvert=0
+      ;;
+
+      #do not process JXL file
+      JXL|jxl)
+        continue
+      ;;
+
+      #this ext list base on image supported by imagemagick
+      AAI|AI|ART|AVIF|AVS|BGR|BGRA|BGRO|BMP|BMP2|BMP3|BRF|CIN|CIP|CUR|CUT|DCM|DICOM|DCRAW|DCX|DDS|DFONT|DOT|DPX|DXT1|DXT5|EPDF|EPI|EPS|EPS2|EPS3|EPSF|EPSI|EPT|EPT2|EPT3|FF|FTP|FTS|FTXT|HEIC|HEIF|HRZ|J2C|J2K|JNG|JNX|JPC|JPE|JPM|JPS|JPT|MIFF|MPC|PCDS|PCL|PCT|PCX|PDB|PES|PFA|PFB|PGM|PHM|PIX|PJPEG|PNM|PS|PS2|PS3|PSB|PSD|PTIF|PWP|QOI|RAS|RGF|RLA|RLE|RMF|SCR|SCT|SFW|SGI|SIX|SVG|SVGZ|TGA|TIF|TIFF|TIM|TM2|TTC|TTF|VIPS|WBMP|WEBM|WEBP|WPG|XCF|XPM|aai|ai|art|avif|avs|bgr|bgra|bgro|bmp|bmp2|bmp3|brf|cin|cip|cur|cut|dcm|dicom|dcraw|dcx|dds|dfont|dot|dpx|dxt1|dxt5|epdf|epi|eps|eps2|eps3|epsf|epsi|ept|ept2|ept3|ff|ftp|fts|ftxt|heic|heif|hrz|j2c|j2k|jng|jnx|jpc|jpe|jpm|jps|jpt|miff|mpc|pcds|pcl|pct|pcx|pdb|pes|pfa|pfb|pgm|phm|pix|pjpeg|pnm|ps|ps2|ps3|psb|psd|ptif|pwp|qoi|ras|rgf|rla|rle|rmf|scr|sct|sfw|sgi|six|svg|svgz|tga|tif|tiff|tim|tm2|ttc|ttf|vips|wbmp|webm|webp|wpg|xcf|xpm)
         extconvert=1  
-        ;;
+      ;;
+
+      # do not process other file
+      *)
+        continue
+      ;;
     esac
 
     icc=$(get_icc_profile_name "$i")
